d86993dae8716256f8d37c054d4a128860c9af82
nobishino <nobishii11@gmail.com>
2020-12-27 11:12:41 +0900

    Refactoring ~ p26

---
+++ src/__tests__/statement.test.js
@@ -0,0 +1,15 @@
+const statement = require("../statement");
+const invoice = require("../data/invoices");
+const plays = require("../data/plays");
+
+test("statement from invoice & plays", () => {
+  expect(statement(invoice[0], plays)).toBe(
+    `Statement for BigCo
+Hamlet: $650.00 (55 seats)
+As You Like It: $580.00 (35 seats)
+Otello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`
+  );
+});
