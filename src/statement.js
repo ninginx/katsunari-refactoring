55fa84b05da0fffcf531bd44880e9ba6090bdcfb
nobishino <nobishii11@gmail.com>
2020-12-27 15:39:36 +0900

    move playFor

--- src/statement.js
+++ src/statement.js
@@ -6,20 +6,20 @@ function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
     return result;
+  }
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
 function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -52,7 +52,7 @@ function renderPlainText(data, plays) {
 
     result += Math.max(aPerformance.audience - 30, 0);
 
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
@@ -75,7 +75,7 @@ function renderPlainText(data, plays) {
   }
 
   for (let perf of data.performances) {
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += `${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
   }
